87449868d7f0a756ba7998679c1f918317a2f996
Guesmi Achref <guesmi.achraf3@gmail.com>
2023-09-30 23:52:52 +0200

    adding property binding

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -11,6 +11,8 @@ export class HomeComponent  implements OnInit {
   etudients:string[]=[];
   mesPosts: any;
   nom:string="mon-composant";
+  logo:string="https://nsm08.casimages.com/img/2013/07/24/1307241027119919611409278.png"
+  displayed: boolean=true;
 constructor(private serviceUser:UsersService, private servicePosts:PostsService){
 
 }
@@ -22,6 +24,10 @@ constructor(private serviceUser:UsersService, private servicePosts:PostsService)
       this.mesPosts= data;
     }
    );
+ }
+ display()
+ {
+  this.displayed=!this.displayed;
  }
 
 }
